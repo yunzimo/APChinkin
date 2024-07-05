017f5685f1b59a8695bbb3a0a35a80cf5f7f5e79
yunzimo <45143713+yunzimo@users.noreply.github.com>
2024-07-05 20:03:06 +0800

    修改备注中的错误，并添加部分日志输出

--- wzyd.js
+++ wzyd.js
@@ -1,7 +1,7 @@
 /*
 cron: 59 15 8 * * *
 王者营地每日签到
-自己抓包找到如下参数并设置到变量WZYD_TOKEN和WZYY_BODY里,多个账号用分号隔开
+自己抓包找到如下参数并设置到变量WZYD_TOKEN和WZYD_BODY里,多个账号用分号隔开
 {"appid": "","openid": "","msdkEncodeParam": "","sig": "","userId": "","source": "","encode": 2,"timestamp": "","algorithm": "v2","version": "3.1.96i"};{"appid": "","openid": "","msdkEncodeParam": "","sig": "","userId": "","source": "","encode": 2,"timestamp": "","algorithm": "v2","version": "3.1.96i"}
 {"cSystem":"ios","h5Get":1,"roleId":"1685189495"};{"cSystem":"ios","h5Get":1,"roleId":"520128481"}
 */
@@ -17,7 +17,9 @@ async function main(){
     	//await version().then(data=>{console.log(data.data)})
 		// 从环境变量中获取Cookie
 		const cookieHeaderValue = process.env.WZYD_TOKEN;
+        console.log(cookieHeaderValue);
         const bodyValue = process.env.WZYD_BODY;
+        console.log(bodyValue);
 		// 将Cookie值拆分为单独的Cookie
 		const cookies = cookieHeaderValue.split(';');
         const body = bodyValue.split(';');
@@ -28,6 +30,7 @@ async function main(){
             axios.post('https://kohcamp.qq.com/operation/action/signin',payload,{headers:header} )
             .then((res) => {
                 notify.sendNotify(payload.roleId+'的王者营地签到结果',JSON.stringify(res.data))
+                console.log(payload.roleId+'的王者营地签到结果',JSON.stringify(res.data))
             }).catch((error) => {
                 console.error(error)
             })
